f68807149b37088df4e1916cef2607ff5301b3b5
Rafael Melo Macieira <rfamm01@gmail.com>
2020-11-27 18:13:25 -0300

    Add files via upload

---
+++ js/knexfile.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+  //development: {
+    client: 'postgresql',
+    connection: {
+      host: 'tdevcgen.macieiras.com.br',
+      database: 'bidweb',
+      user:     'equipe_bidweb',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  /*},
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+*/
+};
